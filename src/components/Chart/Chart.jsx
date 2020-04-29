81ad231f809e9a6482640010a2f49ff86ac63532
CB <32738765+haseebalisajid@users.noreply.github.com>
2020-04-29 16:47:20 +0500

    COVID-19 Tracker APP

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -26,7 +26,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
           datasets: [
             {
               label: 'People',
-              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              backgroundColor: ['rgba(0, 0, 255, 0.7)', 'rgba(0, 255, 0, 0.7)', 'rgba(255, 0, 0, 0.9)'],
               data: [confirmed.value, recovered.value, deaths.value],
             },
           ],
